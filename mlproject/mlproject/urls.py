9fda19981189ae88d743823ed63fbe1991620ff5
upadhyayans <chamanupadhyay50@gmail.com>
2020-02-05 18:09:30 +0530

    ---

--- mlproject/mlproject/urls.py
+++ mlproject/mlproject/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls import include,url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^https://upadhyayans.github.io/$',views.homepage,name='homepage'),
+    url(r'^https://upadhyayans.github.io/tree/master$',views.homepage,name='homepage'),
 ]
